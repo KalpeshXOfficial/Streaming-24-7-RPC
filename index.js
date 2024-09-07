135ee604a9f93fdbed3ad3c2305d4f57cc86896a
KalpeshXOfficial <kalpeshghase144d@gmail.com>
2024-09-07 18:46:29 +0530

    Updated

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ client.on('ready', async () => {
     .setName('Battlegrounds Mobile India')
     .setDetails(`BGMI Playing [${formatTime()}]`)
     .setParty({
-        max: 8130,
-        current: 973,
-        id: Discord.getUUID(),
+      max: 8130,
+      current: 973,
+      id: Discord.getUUID(),
     })
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/1101483858878283877/1281280403860230194/Battleground_Mobile_India.png?ex=66db24dd&is=66d9d35d&hm=0693f1efca4b68772fff09647d93e6f185255a86b142077c6eec4c3c6f4e88a0&=&format=webp&quality=lossless') //You can put links in tenor or discord and etc
+    .setAssetsLargeImage('images\bgmi.png') //You can put links in tenor or discord and etc
     .setAssetsLargeText('Battlegrounds Mobile India') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1101483858878283877/1280947566649016470/verified-verificado.gif?ex=66dbe922&is=66da97a2&hm=9a822e10db0a629b4ebfefa44911c70158bf1f749ed5b097e5739f8c6c426fd2&=') //You can put links in tenor or discord and etc
+    .setAssetsSmallImage('images\verify.gif') //You can put links in tenor or discord and etc
     .setAssetsSmallText('BGMI') //Text when you hover the Small image
     .addButton('My Profile', 'https://www.instagram.com/kalpesh.___3080')
     .addButton('My WebSite', 'https://cyberfoxofficial.blogspot.com');
