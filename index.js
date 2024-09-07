d0bbeaae2705905dfdd9ce0d843837e6dd8dc68f
KalpeshXOfficial <kalpeshghase144d@gmail.com>
2024-09-07 19:35:40 +0530

    Updated

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const client = new Discord.Client({
 const keepAlive = require('./server.js');
 keepAlive();
 
+require('dotenv').config(); // Make sure you have this line to use environment variables
+
 function formatTime() {
   const date = new Date();
   const options = {
-    timeZone: 'Asia/Kolkata', //https://www.zeitverschiebung.net/en/
+    timeZone: 'Asia/Kolkata',
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -22,10 +24,12 @@ client.on('ready', async () => {
   console.clear();
   console.log(`${client.user.tag} - Successfully Logged On!`);
 
+  const activityType = process.env.ACTIVITY_TYPE || 'PLAYING'; // Default to 'PLAYING' if not set
+
   const r = new Discord.RichPresence()
     .setApplicationId('534203414247112723')
-    .setType('PLAYING') //STREAMING, PLAYING, LISTENING
-    .setURL('https://www.youtube.com/@Kalpesh-cc5wn') //Must be a youtube video link 
+    .setType(activityType) // Use the activity type from the environment variable
+    .setURL('https://www.youtube.com/@Kalpesh-cc5wn')
     .setState('Playing With Friends')
     .setName('Battlegrounds Mobile India')
     .setDetails(`BGMI Playing [${formatTime()}]`)
@@ -35,15 +39,17 @@ client.on('ready', async () => {
       id: Discord.getUUID(),
     })
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://tenor.googleapis.com/v2/media?id=2232437380951958796&format=optimizedgif&client_key=tenor_web&appversion=browser-r20240903-1&access_token=ya29.a0AcM612xVDjpdQ3gjn-sdYYzrSdwTy8b-MrZbjR36m7BdxPdRQZosuAFtaOvZLyTqwSXP4uJb9ursuFKU2-QsJDBBrth4JKsybMGszeuAv9ag2OE89F1BosrM8EhX-vDdNSJcM--gfMjQU7MOXkVHsHXBYfnlm6_jowaCgYKAQASARMSFQHGX2Mi96ns4u36RdvPPj_yf8lg1w0169&key=AIzaSyC-P6_qz3FzCoXGLk6tgitZo4jEJ5mLzD8') //You can put links in tenor or discord and etc
-    .setAssetsLargeText('Battleground Mobile India') //Text when you hover the Large image
-    .setAssetsSmallImage('https://tenor.googleapis.com/v2/media?id=15310629048740341586&format=optimizedgif&client_key=tenor_web&appversion=browser-r20240903-1&access_token=ya29.a0AcM612xVDjpdQ3gjn-sdYYzrSdwTy8b-MrZbjR36m7BdxPdRQZosuAFtaOvZLyTqwSXP4uJb9ursuFKU2-QsJDBBrth4JKsybMGszeuAv9ag2OE89F1BosrM8EhX-vDdNSJcM--gfMjQU7MOXkVHsHXBYfnlm6_jowaCgYKAQASARMSFQHGX2Mi96ns4u36RdvPPj_yf8lg1w0169&key=AIzaSyC-P6_qz3FzCoXGLk6tgitZo4jEJ5mLzD8') //You can put links in tenor or discord and etc
-    .setAssetsSmallText('BGMI') //Text when you hover the Small image
+    .setAssetsLargeImage('https://tenor.googleapis.com/v2/media?id=2232437380951958796&format=optimizedgif&client_key=tenor_web&appversion=browser-r20240903-1&access_token=ya29.a0AcM612xVDjpdQ3gjn-sdYYzrSdwTy8b-MrZbjR36m7BdxPdRQZosuAFtaOvZLyTqwSXP4uJb9ursuFKU2-QsJDBBrth4JKsybMGszeuAv9ag2OE89F1BosrM8EhX-vDdNSJcM--gfMjQU7MOXkVHsHXBYfnlm6_jowaCgYKAQASARMSFQHGX2Mi96ns4u36RdvPPj_yf8lg1w0169&key=AIzaSyC-P6_qz3FzCoXGLk6tgitZo4jEJ5mLzD8')
+    .setAssetsLargeText('Battleground Mobile India')
+    .setAssetsSmallImage('https://tenor.googleapis.com/v2/media?id=15310629048740341586&format=optimizedgif&client_key=tenor_web&appversion=browser-r20240903-1&access_token=ya29.a0AcM612xVDjpdQ3gjn-sdYYzrSdwTy8b-MrZbjR36m7BdxPdRQZosuAFtaOvZLyTqwSXP4uJb9ursuFKU2-QsJDBBrth4JKsybMGszeuAv9ag2OE89F1BosrM8EhX-vDdNSJcM--gfMjQU7MOXkVHsHXBYfnlm6_jowaCgYKAQASARMSFQHGX2Mi96ns4u36RdvPPj_yf8lg1w0169&key=AIzaSyC-P6_qz3FzCoXGLk6tgitZo4jEJ5mLzD8')
+    .setAssetsSmallText('BGMI')
     .addButton('My Server', 'https://discord.gg/SVqQYEMp8m')
     .addButton('My Profile', 'https://www.instagram.com/kalpesh.___3080');
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
+  client.user.setPresence({
+    status: "idle"
+  });
 
   let prevTime = null;
   setInterval(() => {
@@ -54,7 +60,7 @@ client.on('ready', async () => {
       client.user.setActivity(r);
       prevTime = newTime;
     }
-  }, 1000); // Update every second
+  }, 1000);
 });
 
 const mySecret = process.env['TOKEN'];
