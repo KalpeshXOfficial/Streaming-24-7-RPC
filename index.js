24a5f5033d2150dbd72c77d70afa5433c5f72be8
KalpeshXOfficial <kalpeshghase144d@gmail.com>
2024-09-09 20:20:29 +0530

    Updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('ready', async () => {
 
   const r = new Discord.RichPresence()
     .setApplicationId('534203414247112723')
-    .setType('PLAYING') //STREAMING, PLAYING, LISTENING
+    .setType('STREAMING') //STREAMING, PLAYING, LISTENING
     .setURL('https://www.youtube.com/@Kalpesh-cc5wn') //Must be a youtube video link 
     .setState('Playing With Friends')
     .setName('Battlegrounds Mobile India')
@@ -35,9 +35,9 @@ client.on('ready', async () => {
       id: Discord.getUUID(),
     })
     .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/1101483858878283877/1281280403860230194/Battleground_Mobile_India.png?ex=66de709d&is=66dd1f1d&hm=14ac8032606c9fba52bf4903ec964d79e34100a6292b45eb5fc04febb59328a8&=&format=webp&quality=lossless') //You can put links in tenor or discord and etc
+    .setAssetsLargeImage('profile') //You can put links in tenor or discord and etc
     .setAssetsLargeText('Battleground Mobile India') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1101483858878283877/1280947566649016470/verified-verificado.gif?ex=66de8c22&is=66dd3aa2&hm=3ab3a1fdb7e335ea6386d40e705d5b2db256961113485623d6e6dc34581e926a&=') //You can put links in tenor or discord and etc
+    .setAssetsSmallImage('pfp') //You can put links in tenor or discord and etc
     .setAssetsSmallText('BGMI') //Text when you hover the Small image
     .addButton('My Server', 'https://discord.gg/SVqQYEMp8m')
     .addButton('My Profile', 'https://www.instagram.com/kalpesh.___3080');
