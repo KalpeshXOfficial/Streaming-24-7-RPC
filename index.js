10250edd03ed74e991f80335b1a9c4d975cbc5f8
KalpeshXOfficial <kalpeshghase144d@gmail.com>
2024-09-09 21:56:03 +0530

    Updated

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const server = express();
 const Discord = require('discord.js-selfbot-v13');
-const axios = require('axios');
-const config = require('./config.json');
-
 const client = new Discord.Client({
   readyStatus: false,
   checkUpdate: false
@@ -12,18 +7,10 @@ const client = new Discord.Client({
 const keepAlive = require('./server.js');
 keepAlive();
 
-const mySecret = process.env['TOKEN'];
-const statuses = config.statuses;
-let status_index = 0;
-
-server.all("/", (req, res) => {
-  res.send('<meta http-equiv="refresh" content="0; URL=https://mrdarkshadoow.github.io"/>');
-});
-
 function formatTime() {
   const date = new Date();
   const options = {
-    timeZone: 'Asia/Kolkata',
+    timeZone: 'Asia/Kolkata', //https://www.zeitverschiebung.net/en/
     hour12: true,
     hour: 'numeric',
     minute: 'numeric'
@@ -37,26 +24,27 @@ client.on('ready', async () => {
 
   const r = new Discord.RichPresence()
     .setApplicationId('1280906241501036544')
-    .setType('STREAMING')
-    .setURL('https://www.twitch.tv/discord')
-    .setState('Streaming with Squad')
-    .setName('Battlegrounds Mobile India')
-    .setDetails(`Streaming BGMI [${formatTime()}]`)
+    .setType('STREAMING') //STREAMING, PLAYING, LISTENING
+    .setURL('https://www.twitch.tv/discord') // Twitch link required for STREAMING type
+    .setState('Streaming with Squad') // Custom status message
+    .setName('Battlegrounds Mobile India') // Custom game name
+    .setDetails(`Streaming BGMI [${formatTime()}]`) // Custom details with a dynamic time
     .setParty({
       max: 8130,
       current: 973,
-      id: Discord.getUUID(),
+      id: Discord.getUUID(), // Party ID for the session
     })
-    .setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/1101483858878283877/1281280403860230194/Battleground_Mobile_India.png?ex=66dfc21d&is=66de709d&hm=522e9c0b8070d61c4dc2fd22a122006c3f97c4341bcdda3e69ae09a5727328df&=&format=webp&quality=lossless')
-    .setAssetsLargeText('Battleground Mobile India')
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1101483858878283877/1280947566649016470/verified-verificado.gif?ex=66dfdda2&is=66de8c22&hm=e237c83dcd3fa1cd3eef2910c897b97d9766472c415f043325ebaa55e0abb977&=')
-    .setAssetsSmallText('Verified')
-    .addButton('Join My Discord', 'https://discord.gg/SVqQYEMp8m')
-    .addButton('Follow My Instagram', 'https://www.instagram.com/kalpesh.___3080');
+    .setStartTimestamp(Date.now()) // Starts the timestamp from now
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1101483858878283877/1281280403860230194/Battleground_Mobile_India.png?ex=66dfc21d&is=66de709d&hm=522e9c0b8070d61c4dc2fd22a122006c3f97c4341bcdda3e69ae09a5727328df&=&format=webp&quality=lossless') // Large image for the game
+    .setAssetsLargeText('Battleground Mobile India') // Text on hover for large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1101483858878283877/1280947566649016470/verified-verificado.gif?ex=66dfdda2&is=66de8c22&hm=e237c83dcd3fa1cd3eef2910c897b97d9766472c415f043325ebaa55e0abb977&=') // Small verified icon
+    .setAssetsSmallText('Verified') // Text on hover for small image
+    .addButton('Join My Discord', 'https://discord.gg/SVqQYEMp8m') // First custom button
+    .addButton('Follow My Instagram', 'https://www.instagram.com/kalpesh.___3080'); // Second custom button
+
 
   client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" });
+  client.user.setPresence({ status: "idle" }); // online, idle, dnd, offline
 
   let prevTime = null;
   setInterval(() => {
@@ -67,28 +55,8 @@ client.on('ready', async () => {
       client.user.setActivity(r);
       prevTime = newTime;
     }
-  }, 1000);
-
-  setInterval(() => {
-    axios.patch('https://discordapp.com/api/v6/users/@me/settings', {
-      custom_status: {
-        text: statuses[status_index++ % statuses.length],
-        expires_at: null
-      }
-    }, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36',
-        'Authorization': mySecret,
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(() => {
-        console.log('Status updated');
-      })
-      .catch(err => {
-        console.error('Error updating status:', err);
-      });
-  }, config.time);
+  }, 1000); // Update every second
 });
 
+const mySecret = process.env['TOKEN'];
 client.login(mySecret);
